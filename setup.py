94c8abbd3cb630714770165a01b8b6cbd09325bf
Alpha <alpha@pokesplash.net>
2014-11-23 18:15:46 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='newdispatch',
+      version='1.0',
+      description='Async Handler and Event Dispatcher',
+      url='http://github.com/Alphadelta14/python-newdispatch',
+      author='Alpha',
+      author_email='alpha@projectpokemon.org',
+      license='MIT',
+      packages=['dispatch'],
+      zip_safe=True)
