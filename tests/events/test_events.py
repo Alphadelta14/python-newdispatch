e8c324f5bce417ea6a6c8295c7a58edef29d91b5
Alpha <alpha@pokesplash.net>
2014-12-07 12:24:24 -0500

    Added tests for EventData.cancel()

--- tests/events/test_events.py
+++ tests/events/test_events.py
@@ -44,3 +44,77 @@ class TestEvents(unittest.TestCase):
 
         emitter.fire('event:order')
         self.assertEqual(order, [2, 1])
+
+    def test_cancel(self):
+        order = []
+
+        emitter = Emitter()
+
+        @emitter.on('event:cancel')
+        def callback(evt):
+            order.append(1)
+            evt.cancel()
+            order.append(2)
+
+        @emitter.on('event:cancel')
+        def callback(evt):
+            order.append(3)
+
+        emitter.fire('event:cancel')
+        self.assertEqual(order, [1])
+
+    def test_cancel_nested(self):
+        order = []
+
+        emitter = Emitter()
+
+        @emitter.on('event:cancel')
+        def callback(evt):
+            order.append(1)
+
+            @emitter.on('event:cancel_nested')
+            def callback_nested(subevt):
+                order.append(2)
+                evt.cancel()
+                order.append(3)
+
+            @emitter.on('event:cancel_nested')
+            def callback_nested(subevt):
+                order.append(4)
+            emitter.fire('event:cancel_nested')
+            order.append(5)
+
+        @emitter.on('event:cancel')
+        def callback(evt):
+            order.append(6)
+
+        emitter.fire('event:cancel')
+        self.assertEqual(order, [1, 2])
+
+    def test_cancel_nested2(self):
+        order = []
+
+        emitter = Emitter()
+
+        @emitter.on('event:cancel')
+        def callback(evt):
+            order.append(1)
+
+            @emitter.on('event:cancel_nested')
+            def callback_nested(subevt):
+                order.append(2)
+                subevt.cancel()
+                order.append(3)
+
+            @emitter.on('event:cancel_nested')
+            def callback_nested(subevt):
+                order.append(4)
+            emitter.fire('event:cancel_nested')
+            order.append(5)
+
+        @emitter.on('event:cancel')
+        def callback(evt):
+            order.append(6)
+
+        emitter.fire('event:cancel')
+        self.assertEqual(order, [1, 2, 5, 6])
