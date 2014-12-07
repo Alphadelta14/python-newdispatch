e8865d15d0168c72b7c99425cb0dfddc05ddbfc7
Alpha <alpha@pokesplash.net>
2014-12-07 13:16:33 -0500

    Bumping up version for changes in API

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='newdispatch',
-      version='1.0',
+      version='1.2',
       description='Async Handler and Event Dispatcher',
       url='http://github.com/Alphadelta14/python-newdispatch',
       author='Alpha',
