9d386ae0aa56af1bca38d9f732ff718a6642779c
Alpha <alpha@pokesplash.net>
2015-01-03 02:54:10 -0500

    Package submodules in setup script

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup
 
 setup(name='newdispatch',
-      version='1.2',
+      version='1.2.1',
       description='Async Handler and Event Dispatcher',
       url='http://github.com/Alphadelta14/python-newdispatch',
       author='Alpha',
       author_email='alpha@projectpokemon.org',
       license='MIT',
-      packages=['dispatch'],
+      packages=['dispatch', 'dispatch.events', 'dispatch.async'],
       zip_safe=True)
